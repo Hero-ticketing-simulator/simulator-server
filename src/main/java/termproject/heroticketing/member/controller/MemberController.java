aee5fe42bada47309e5b86c530f2ee0a04759b76
student-p <paulmoon00@naver.com>
2024-11-23 14:55:12 +0900

    fix: MemberController 수정

--- src/main/java/termproject/heroticketing/member/controller/MemberController.java
+++ src/main/java/termproject/heroticketing/member/controller/MemberController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.Controller;
 import termproject.heroticketing.member.dto.SignUpForm;
 import termproject.heroticketing.member.service.MemberService;
 
